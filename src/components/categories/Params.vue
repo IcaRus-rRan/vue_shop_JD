b0e8f74eecaf07a66a845501f688a0145993014b
IcaRus-rRan <254780245@qq.com>
2020-10-25 16:17:39 +0800

    更新组件所在文件夹

---
+++ src/components/categories/Params.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+	<div>
+		<!-- 面包屑导航 -->
+		<el-breadcrumb separator-class="el-icon-arrow-right" class="el-breadcrumb">
+      <el-breadcrumb-item :to="{ path: '/Home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+		}
+	}
+}
+</script>
+
+<style  scoped>
+
+</style>
